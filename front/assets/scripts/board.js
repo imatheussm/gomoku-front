382704731afb0256353c04a3e000a93078ecea2b
Gabriela Maximino <39780413+gabrielamaximino@users.noreply.github.com>
2021-09-16 15:00:02 -0300

    rascunho da lógica do jogo
    jogo funcionando, porém otimizações são necessárias.
    - Método didGameEnd() e checkCounter()  adicionados.

--- front/assets/scripts/board.js
+++ front/assets/scripts/board.js
@@ -65,15 +65,145 @@ class Board{
         this.gamestate[x][y] = this.currentPlayer
         event.target.className += this.currentPlayer < 0 ? ' blue' : ' red'
 
+        if (this.didGameEnd([x, y])) {
+            alert("Fim de jogo!")
+        } 
+
         this.currentPlayer = -this.currentPlayer;
-
-        this.didGameEnd()
-
     }
 
-    didGameEnd()
+    didGameEnd(position)
     {
-        return false;
+        let [x, y] = position
+
+        const horizontal_sides = {
+            right(x, y) {
+                return [x, ++y]
+            },
+            left(x, y) {
+                return [x, --y]
+            }
+        }
+
+        const vertical_sides = {
+            top(x, y) {
+                return [--x, y]
+            },
+            bottom(x, y) {
+                return [++x, y]
+            }
+        }
+
+        const main_diagonal = {
+            diagonal_left_top(x, y) {
+                return [--x, --y]
+            },
+            diagonal_right_bottom(x, y) {
+                return [++x, ++y]
+            }
+
+        }
+
+        const secondary_diagonal = {
+            diagonal_right_top(x, y) {
+                return [--x, ++y]
+            },
+            diagonal_left_bottom(x, y) {
+                return [++x, --y]
+            }
+        }
+
+        //console.log("Posição atual:" + [x, y])
+
+        let stone_counter = 1
+        let [current_x, current_y] = [x, y]
+        
+        try {
+            // Checking horizontal sides (left and right)
+            for (let side of Object.values(horizontal_sides)) {
+                [x, y] = side(current_x, current_y)
+
+                while (this.gamestate[x][y] == this.currentPlayer) {
+                    stone_counter++;
+
+                    [x, y] = side(x, y)
+
+                    //console.log("Contador horizontal: " + stone_counter)
+
+                    if (stone_counter == 5) break;
+
+                }
+            }
+
+            if (this.checkCounter(stone_counter)) return true;
+
+            stone_counter = 1
+
+            // Checking vertical sides (top and bottom)
+            for (let side of Object.values(vertical_sides)) {
+                [x, y] = side(current_x, current_y)
+
+                while (this.gamestate[x][y] == this.currentPlayer) {
+                    stone_counter++;
+
+                    [x, y] = side(x, y)
+
+                    //console.log("Contador horizontal: " + stone_counter)
+
+                    if (stone_counter == 5) break;
+
+                }
+            }
+
+            if (this.checkCounter(stone_counter)) return true;
+
+            stone_counter = 1
+
+            // Checking main diagonal sides 
+            for (let side of Object.values(main_diagonal)) {
+                [x, y] = side(current_x, current_y)
+
+                while (this.gamestate[x][y] == this.currentPlayer) {
+                    stone_counter++;
+
+                    [x, y] = side(x, y)
+
+                    console.log("Contador da diagonal : " + stone_counter)
+
+                    if (stone_counter == 5) break;
+
+                }
+            }
+
+            if (this.checkCounter(stone_counter)) return true;
+
+            stone_counter = 1
+
+            // Checking secondary diagonal sides 
+            for (let side of Object.values(secondary_diagonal)) {
+                [x, y] = side(current_x, current_y)
+
+                while (this.gamestate[x][y] == this.currentPlayer) {
+                    stone_counter++;
+
+                    [x, y] = side(x, y)
+
+                    console.log("Contador da diagonal secundária: " + stone_counter)
+
+                    if (stone_counter == 5) break;
+
+                }
+            }
+
+            if (this.checkCounter(stone_counter)) return true;
+
+        } catch(e) {
+            console.log(e)
+        }
+    } 
+
+    checkCounter(counter) {
+        return (counter == 5)
     }
 
 
