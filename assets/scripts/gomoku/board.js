4f2ffe04a4a9d51c0a486db43201bfa34113964e
Igor Matheus Moreira <imatheus.sm@gmail.com>
2021-10-05 14:17:11 -0300

    feat: add dark mode version of logo

--- assets/scripts/gomoku/board.js
+++ assets/scripts/gomoku/board.js
@@ -90,8 +90,12 @@ export class Board
         let themeStylesheets = document.getElementsByClassName("theme-stylesheet")
         let boardStylesheets = document.getElementsByClassName("board-stylesheet")
         let isDark = document.getElementById("theme-button").checked
+
         let newThemeStylesheet = `assets/style/bootstrap-5.1.1${isDark? "-night" : ""}.min.css`
-        let newBoardStylesheet = `assets/style/board${isDark? "-night" : ""}.css`
+        let newBoardStylesheet = `assets/style/board${isDark ? "-night" : ""}.css`
+        let newLogoImage = `assets/images/gomoku-logo${isDark ? "-night" : ""}.png`
+
+        document.getElementById("logo-image").setAttribute("src", newLogoImage)
 
         for (let i = 0; i < themeStylesheets.length; i++) themeStylesheets.item(i)
             .setAttribute("href", newThemeStylesheet)
